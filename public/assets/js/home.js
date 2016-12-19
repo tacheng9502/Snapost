4845df29280f865ca5684198e52b769b75ea8456
g6165310 <g6165310@gmail.com>
2016-12-20 00:41:27 +0800

    修改圖片縮圖

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -133,7 +133,7 @@
               '<img id="' + array[i].postKey + '_postImage" width="600px" src="' + array[i].postImage + '"/>' +
               '</li>'
             );
-            jQuery('#'+array[i].postKey).nailthumb({width:500,height:500,method:'resize',fitDirection:'center'});
+            jQuery('#'+array[i].postKey+ '_postImage').nailthumb({width:500,height:500,method:'resize',fitDirection:'center'});
           } else {
             $('#list').append(
               '<li>' +
@@ -149,7 +149,7 @@
               '<img id="' + array[i].postKey + '_postImage" width="600px" src="' + array[i].postImage + '"/>' +
               '</li>'
             );
-            jQuery('#'+array[i].postKey).nailthumb({width:600,height:600,method:'resize',fitDirection:'center'});
+            jQuery('#'+array[i].postKey+ '_postImage').nailthumb({width:600,height:600,method:'resize',fitDirection:'center'});
           }
         }
       }, function (errorObject) {
