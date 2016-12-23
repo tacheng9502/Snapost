feb299fe88de07af0c1f062622c3f43486a164b8
Tacheng <tacheng9502@gmail.com>
2016-12-23 21:54:36 +0800

    修正程式錯誤

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -41,7 +41,6 @@ jQuery(document).ready(function ($) {
 
         var postsRef = firebase.database().ref('posts').limitToLast(50);
         postsRef.once("value", function (snapshot) {
-            $('#list').children().remove();
             snapshot.forEach(function (data) {
                 var html = createPostElement(data.key, data.val().userId, data.val().userName, data.val().userImage, data.val().postBody, data.val().postTime, data.val().postImage, data.val().likeCount);
                 $(html).insertBefore($("#list:first-child"));
