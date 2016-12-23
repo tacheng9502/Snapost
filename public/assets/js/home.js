aa6fd0f4fe9d274347c3e3ce94697d882c188b28
Tacheng <tacheng9502@gmail.com>
2016-12-23 17:01:37 +0800

    修正程式錯誤

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -39,7 +39,7 @@ jQuery(document).ready(function ($) {
 
     function startDatabaseQueries() {
 
-        var recentPostsRef = firebase.database().ref('posts').limitToLast(50);
+        var postsRef = firebase.database().ref('posts').limitToLast(50);
         postsRef.ref('posts').once("value", function (snapshot) {
             $('#list').children().remove();
             var html = createPostElement(data.key, data.val().userId, data.val().userName, data.val().userImage, data.val().postBody, data.val().postTime, data.val().postImage, data.val().likeCount);
