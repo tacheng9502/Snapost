0f1a52d4a0d392385ee74f49b3bb9f1b109a39a0
Tacheng <tacheng9502@gmail.com>
2016-12-25 01:16:27 +0800

    修正廣告顯示

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -167,7 +167,7 @@ jQuery(document).ready(function ($) {
             });
 
             for (var i = 1; i <= array.length; i++) {
-                $("#list li:nth-child("+(3*i+1)+")").prepend(array[i-1]);
+                $("#list li:nth-child("+(3*i)+")").after(array[i-1]);
             }
         });
     }
