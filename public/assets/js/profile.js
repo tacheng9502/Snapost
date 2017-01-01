c771127ba37aa1c0195c7c229f343903d5d80c22
Simon <simonnyu@gmail.com>
2017-01-02 01:45:18 +0800

    修正個人頁錯誤

--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -41,7 +41,7 @@ jQuery(document).ready(function($) {
         var postRef = firebase.database().ref('users/' + queryId + '/userPost');
         var ninebox_html = ""
         postRef.once('value', function(data) {
-            $("ninebox").empty();
+            $("#ninebox").empty();
             data.forEach(function(childdata) {
                 var postKey = childdata.key;
                 var postImage = childdata.val();
@@ -51,6 +51,7 @@ jQuery(document).ready(function($) {
                     '</li>' + ninebox_html;
             });
         });
+        console.log(ninebox_html);
         $("#ninebox").append(ninebox_html);
 
         if (queryId != currentUserId) {
