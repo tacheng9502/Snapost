4372d49ddadabfd0d3ad0b96d818e098fca4a284
Tacheng <tacheng9502@gmail.com>
2017-01-02 16:43:24 +0800

    新增動態仔入

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -563,7 +563,7 @@ jQuery(document).ready(function ($) {
             postsRef.on('child_added', function (data) {
                 if (!followLastPost.includes(data.key)) {
                     var html = createPostElement(data.key, data.val().userId, data.val().userName, data.val().userImage, data.val().postBody, data.val().postTime, data.val().postImage, data.val().likeCount);
-                    $('#list').prepend(html);
+                    $('#list').append(html);
                 }
             });
             postsRef.on('child_changed', function (data) {
