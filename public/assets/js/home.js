822e404fddeb837cba193968c1aa7a2355136463
Tacheng <tacheng9502@gmail.com>
2017-01-02 23:04:59 +0800

    修正程式錯誤

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -557,15 +557,14 @@ jQuery(document).ready(function ($) {
     }
 
     win.scroll(function () {
-
+        var lastPostId = $('li.post:last').attr('id');
+        console.log(lastPostId);
         if ($(document).height() - window.innerHeight == win.scrollTop() && loadController) {
             loadController = false;
             console.log('到底部囉');
-            var lastPostId = $('li.post:last').attr('id');
-            console.log(lastPostId);
             var postsRef = firebase.database().ref('posts').orderByKey().endAt(lastPostId).limitToLast(8);
             postsRef.on('child_added', function (data) {
-                if (!followLastPost.includes(data.key)) {
+                if (!followLastPost.includes(data.key) && lastPostId!=data.key) {
                     var html = createPostElement(data.key, data.val().userId, data.val().userName, data.val().userImage, data.val().postBody, data.val().postTime, data.val().postImage, data.val().likeCount);
                     $("#list").append(html);
                 }
