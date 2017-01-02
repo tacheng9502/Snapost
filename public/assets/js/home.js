17ac20363c641a6287665b031443875158d0999e
Tacheng <tacheng9502@gmail.com>
2017-01-02 23:10:31 +0800

    修正程式錯誤

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -565,7 +565,7 @@ jQuery(document).ready(function ($) {
             postsRef.on('child_added', function (data) {
                 if (!followLastPost.includes(data.key) && lastPostId!=data.key) {
                     var html = createPostElement(data.key, data.val().userId, data.val().userName, data.val().userImage, data.val().postBody, data.val().postTime, data.val().postImage, data.val().likeCount);
-                    $("#"+lastPostId).prepend(html);
+                    $("#"+lastPostId).after(html);
                 }
             });
             postsRef.on('child_changed', function (data) {
