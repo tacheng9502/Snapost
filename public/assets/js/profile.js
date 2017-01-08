9503da46fa95178903c194621ca02948ac1ff702
beibei0221 <ya0981012155@gmail.com>
2017-01-09 00:22:33 +0800

    修改彈跳式視窗樣式

--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -553,10 +553,8 @@ jQuery(document).ready(function($) {
                     text: html,
                     html: true
                 });
-            $('.sweet-overlay').on('click', function (event) {
-                swal.close();
-            });
-            $(".showSweetAlert").addClass("alertPost");
+
+            $(".showSweetAlert").addClass("alertBody");
         });
     };
 })
