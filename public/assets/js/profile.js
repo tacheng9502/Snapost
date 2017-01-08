040349dc8d3de4b6229938f10eb4c32fd84bce14
Simon <simonnyu@gmail.com>
2017-01-08 16:30:15 +0800

    修改個人頁程式碼

--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -504,14 +504,14 @@ jQuery(document).ready(function($) {
 
     window.clickImg = function(event) {
         event.preventDefault();
-        console.console.log("ZZZ");
+        console.log("ZZZ");
         var refKey = event.target.id.slice(0, -10);
         var postDetailRef = firebase.database().ref('posts/' + refKey + '/');
         postDetailRef.on('value', function(data) {
-            console.console.log("!!!");
+            console.log("!!!");
             var html = createPostElement(refKey, data.val().userId, data.val().userName, data.val().userImage, data.val().postBody, data.val().postTime, data.val().postImage, data.val().likeCount);
             function alert() {
-                console.console.log("?!");
+                console.log("?!");
                 swal({
                     title: "",
                     text: html,
