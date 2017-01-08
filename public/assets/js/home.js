d653ffef5f7c90ae204166de7880429fa99099e4
Tacheng <tacheng9502@gmail.com>
2017-01-09 01:07:30 +0800

    修正修改以及刪除貼文導致的標籤殘留問題

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -438,7 +438,7 @@ jQuery(document).ready(function ($) {
         var postKey = event.target.id.slice(0, -5);
         var postBody = stripHTML($('#' + postKey + '_newBody').val());
 
-        var matched = postBody.match(/(^#\S+)|(\s+#\S+)/g);
+        var matched = postBody.match(/(\S+#\S+)|(\s+#\S+)/g);
         if (matched != null) {
             [].forEach.call(matched, function (matchText) {
                 var hashtagName = matchText.split("#");
@@ -514,7 +514,7 @@ jQuery(document).ready(function ($) {
                 closeOnConfirm: false
             },
             function () {
-                var matched = oldBody.match(/(^#\S+)|(\s+#\S+)/g);
+                var matched = oldBody.match(/(\S+#\S+)|(\s+#\S+)/g);
                 if (matched != null) {
                     [].forEach.call(matched, function (matchText) {
                         var hashtagName = matchText.split("#");
