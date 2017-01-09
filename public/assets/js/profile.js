286f259fe237e94fb2c78ee4b3cb8b69e700c434
beibei0221 <ya0981012155@gmail.com>
2017-01-09 16:20:53 +0800

    修改sweetalert Escape鍵

--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -596,7 +596,8 @@ jQuery(document).ready(function ($) {
           title: "",
           text: html,
           html: true,
-          showConfirmButton: false
+          showConfirmButton: false,
+          allowEscapeKey: false
       });
       $('.sweet-overlay').on('click', function (event) {
           swal.close();
