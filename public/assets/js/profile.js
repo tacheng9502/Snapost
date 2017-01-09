3763111c4c268d12b136d5e945e3b2ee7b8b2dd8
Simon <simonnyu@gmail.com>
2017-01-09 16:17:18 +0800

    修正個人頁彈跳視窗錯誤

--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -585,7 +585,6 @@ jQuery(document).ready(function ($) {
         var html = "";
         postDetailRef.on('value', function (data) {
             if(viewListener == false){
-                console.log("~!~");
                 createSweetAlertView(createPostElement(refKey, data.val().userId, data.val().userName, data.val().userImage, data.val().postBody, data.val().postTime, data.val().postImage, data.val().likeCount));
                 getComment(refKey);
                 viewListener = true;
@@ -597,28 +596,17 @@ jQuery(document).ready(function ($) {
           title: "",
           text: html,
           html: true,
-          showConfirmButton: false,
-          allowEscapeKey: false
+          showConfirmButton: false
       });
       $('.sweet-overlay').on('click', function (event) {
           swal.close();
           $('.showSweetAlert').remove();
           $('.alertBody').remove();
           $('.sweet-overlay').remove();
-          ('.sweet-alert').remove();
+          $('.sweet-alert').remove();
           viewListener = false;
       });
-      $(document).keydown(function(e) {
-          // ESCAPE key pressed
-          if (e.keyCode == 27) {
-              swal.close();
-              $('.sweet-alert').remove();
-              $('.showSweetAlert').remove();
-              $('.alertBody').remove();
-              $('.sweet-overlay').remove();
-              viewListener = false;
-          }
-      });
+
       $(".showSweetAlert").addClass("alertBody");
     }
 
